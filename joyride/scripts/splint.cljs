f55fd54affd3a5fe6558b854ab252fd7c2e6e58c
Sean Corfield <sean@corfield.org>
2025-07-02 12:27:23 -0400

    Switch to bb for running Splint (faster)

--- joyride/scripts/splint.cljs
+++ joyride/scripts/splint.cljs
@@ -1,6 +1,13 @@
 (ns splint
   (:require ["vscode" :as vscode]))
 
+(def ^:private splint-command
+  "The command to run Splint. If you have `bb` installed, you can use
+   the first definition, otherwise use the second one."
+  "bb -Sdeps '{:deps {io.github.noahtheduke/splint {:mvn/version \"1.21.0\"}}}' -m noahtheduke.splint"
+  #_
+  "clojure -M:splint")
+
 (defn- find-or-create-terminal [name]
   (or (some #(when (= name (.-name %)) %) vscode/window.terminals)
       (vscode/window.createTerminal #js {:name name})))
@@ -8,4 +15,4 @@
 (let [file vscode/window.activeTextEditor.document.fileName]
   (doto (find-or-create-terminal "Splint")
     (.show)
-    (.sendText (str "clojure -M:splint -o simple " file))))
+    (.sendText (str splint-command " -o simple " file))))
