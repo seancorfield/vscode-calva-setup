6ca44d6bf3ac35cd6a36176e103f7a07552ec454
Sean Corfield <sean@corfield.org>
2026-07-28 21:12:27 -0400

    update splint version
    Signed-off-by: Sean Corfield <sean@corfield.org>

--- joyride/scripts/splint.cljs
+++ joyride/scripts/splint.cljs
@@ -4,7 +4,7 @@
 (def ^:private splint-command
   "The command to run Splint. If you have `bb` installed, you can use
    the first definition, otherwise use the second one."
-  "bb -Sdeps '{:deps {io.github.noahtheduke/splint {:mvn/version \"1.21.0\"}}}' -m noahtheduke.splint"
+  "bb -Sdeps '{:deps {io.github.noahtheduke/splint {:mvn/version \"1.24.0\"}}}' -m noahtheduke.splint"
   #_
   "clojure -M:splint")
 
